0cda8e37434492c01d75053213b001a542851962
Manoel Rebelo Abranches <abranches@gmail.com>
2016-03-29 09:20:31 -0300

    ao popular array ou hash com csv é intanciado objeto correspondente

--- lib/csv_source.rb
+++ lib/csv_source.rb
@@ -1,36 +1,36 @@
 module Source
   require 'csv'
   #read cupons from csv location
-  class CSVsource
-   attr_accessor :location
+  class CSVSource
+    attr_accessor :location
 
-   def initialize(location)
-     @location = location
-   end 
+    def initialize(location=nil)
+      @location = location
+    end 
 
-   def populate_array
-     array = []
-     read(@location) do |row|
-       array << klass.new(*row)
-     end
-     hash   
-   end
+    def populate_array(klass)
+      array = []
+      read(@location) do |row|
+        array << klass.new(*row)
+      end
+      array 
+    end
 
-   def populate_hash
-     hash = {}
-     read(@location) do |row|
-       hash[row[0]] = klass.new(*row)
-     end
-     hash
-   end
+    def populate_hash(klass)
+      hash = {}
+      read(@location) do |row|
+        hash[row[0].to_i] = klass.new(*row)
+      end
+      hash
+    end
 
-   private
+    private
 
-   def read(location)  
-     CSV.foreach(location) do |row|
-       yield row if block_given?
-     end
-   end
+    def read(location)  
+      CSV.foreach(location) do |row|
+        yield row if block_given?
+      end
+    end
 
- end
+  end
 end

f088490f0d3126a2a7309233be50bedb8a895982
Manoel Rebelo Abranches <abranches@gmail.com>
2016-03-30 00:06:03 -0300

    teste para classe order

---
+++ spec/models/order_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Order do
+  before(:each) do
+    @order = Order.new(1)   
+    @coupon = Coupon.new(1, 25, Coupon::PERCENT, (Date.today + 100).strftime("%Y/%m/%d"), 1)
+    
+  end
+
+  it "should use discount from coupon if it is better" do
+     (1..3).each do |id|
+        @order.add_product(Product.new(id,100))
+     end
+     @order.compute(@coupon)
+     expect(@order.final_value).to eq(225)
+  end 
+  
+  it "should use progressive discount if it is better" do
+     (1..8).each do |id|
+        @order.add_product(Product.new(id,100))
+     end
+     @order.compute(@coupon)
+     expect(@order.final_value).to eq(480)
+  end 
+ 
+  it "should not be able to use the same product twice" do
+  end
+ 
+end
